c60cf0b2da591dffc86204195032aee07b6a6bf5
Haastrup ELijah <haastrupea@gmail.com>
2021-01-17 12:10:38 +0100

    add documentation route and npm start command

--- route/rate-converter.js
+++ route/rate-converter.js
@@ -8,6 +8,7 @@ const serverResp = (resData, res) => {
     results: resData.data,
   });
 };
+
 route.get("/api/rates", (req, res) => {
   //get all params
   const base = req.query.base;
@@ -17,7 +18,7 @@ route.get("/api/rates", (req, res) => {
       status: 400,
       data: {
         callSample:
-          "http://localhost:5000/api/rates?base=CZJ&currency=EUR,GBP,USD",
+          "https://currecncy-exchange.herokuapp.com/api/rates?base=CZJ&currency=EUR,GBP,USD",
       },
     };
     paramError.data.error = "Missing parameters: `base` or `currency`";
@@ -37,5 +38,17 @@ route.get("/api/rates", (req, res) => {
       return serverResp(err, res);
     });
 });
+
+route.get('*',(req,res)=>{
+    const paramError = {
+        status: 400,
+        data: {
+            message:"ops!!!. Find sample of how to call this Api in data.callSample property of this Object",
+          callSample:
+            "https://currecncy-exchange.herokuapp.com/api/rates?base=CZJ&currency=EUR,GBP,USD",
+        },
+      }
+    return serverResp(paramError,res)
+})
 
 module.exports = route;
